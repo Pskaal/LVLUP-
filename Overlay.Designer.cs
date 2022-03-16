56cf9aaaec1b0d13d35a34804e68a583bd5be548
Pskaal <96086612+Pskaal@users.noreply.github.com>
2022-03-16 23:32:13 +0100

    Add project files.

---
+++ Overlay.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace LVLUP_
+{
+    partial class Overlay
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(846, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Prt sc";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Overlay
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(1520, 100);
+            this.ControlBox = false;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.KeyPreview = true;
+            this.Location = new System.Drawing.Point(200, 0);
+            this.MaximizeBox = false;
+            this.MdiChildrenMinimizedAnchorBottom = false;
+            this.MinimizeBox = false;
+            this.Name = "Overlay";
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button button1;
+        private Label label2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
---
+++ Overlay.cs
@@ -0,0 +1,63 @@
+using Tesseract;
+using System;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Printing;
+
+
+namespace LVLUP_
+{
+    public partial class Overlay : Form
+    {
+        public Overlay()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+/*            while (true)
+            {*/
+
+                int xSize = 300;
+                int ySize = 35;
+                Bitmap bitmap = new Bitmap(xSize, ySize);
+                Graphics g = Graphics.FromImage(bitmap as Image);
+                g.CopyFromScreen(new Point((Screen.PrimaryScreen.Bounds.Width - xSize) / 2, Screen.PrimaryScreen.Bounds.Height - 205), new Point(0, 0), new Size(xSize, ySize));
+                bitmap.Save("C:/Users/admin/source/repos/LVLUP!/TEMP/temp.jpg");
+                var Result = new IronOcr.IronTesseract().Read("C:/Users/admin/source/repos/LVLUP!/TEMP/temp.jpg");
+                string currentSubText = Result.Text;
+
+                label2.Text = currentSubText;
+
+                if (currentSubText.Contains("Coast"))
+                {
+                    label1.Text = "Go to waypoint and enter The Mud Flats";
+                }
+                if (currentSubText.Contains("Lion"))
+                {
+                    label1.Text = "Hump Nessa for some sexy gems";
+                }
+                
+/*            }
+            Thread.Sleep(3000);*/
+
+
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
